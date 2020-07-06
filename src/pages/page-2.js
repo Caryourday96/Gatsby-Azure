aeefa0c70e0325b64b33bc0120baf00b930f7a52
Kayode <adetkay@gmail.com>
2020-07-06 11:00:12 -0400

    added files and download link

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,14 +3,25 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import downloadFile from '../images/TDOHB.pdf'
+import downloadFile1 from '../images/TDOHBS.pdf'
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+
+const SecondPage = () => ( <
+    Layout >
+    <
+    SEO title = "Page two" / >
+    <
+    h1 > Hi from the second page < /h1> <
+    p > Welcome to page 2 < /p> <
+    a href = { downloadFile }
+    download > Kayode 1 < /a> <
+    a href = { downloadFile1 }
+    download > Kayode 2 < /a>
+
+    <
+    Link to = "/" > Go back to the homepage < /Link> <
+    /Layout>
 )
 
 export default SecondPage
