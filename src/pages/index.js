895f195ae92213c2dce4272d3746e0c2742fc11b
Adetunji Kayode <adetkay@gmail.com>
2020-08-12 09:55:20 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => ( <
     SEO title = "Home" / >
     <
     h1 > Hi people < /h1> <
-    p > Welcome to Kayode 's new Gatsby site.</p> <
+    p > Welcome to Adetunji Kayode 's new Gatsby site.</p> <
     p > Now go build something great. < /p> <
     div style = {
         { maxWidth: `300px`, marginBottom: `1.45rem` } } >
